8eefb67b0eab4ef013fbe55fedc06b9305c429a7
IT-liguiyang <1781605904@qq.com>
2022-02-15 16:34:32 +0800

    feat: 实现意见建议模块接口

---
+++ models/OpinionsSuggestionsModel.js
@@ -0,0 +1,20 @@
+/*
+能操作news集合数据的Model
+ */
+// 1.引入mongoose
+const mongoose = require('mongoose');
+
+// 2.字义Schema(描述文档结构)
+const opinions_suggestionsSchema = new mongoose.Schema({
+  publisher: String, // 发布人
+  pub_time: String, // 发布时间
+  pub_theme: String, // 公告主题
+  pub_content: Array, // 内容
+  create_time: {type: Number, default:new Date()} 
+});
+
+// 3. 定义Model(与集合对应, 可以操作集合)
+const OpinionsSuggestionsModel = mongoose.model('opinions_suggestions', opinions_suggestionsSchema);
+
+// 4. 向外暴露Model
+module.exports = OpinionsSuggestionsModel;
